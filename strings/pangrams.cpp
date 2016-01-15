235dd13d45a7919412a930791d802f7eb3d20a2e
Brendan Creane <bcreane@riverbed.com>
2016-01-14 22:08:49 -0800

    pangrams!

--- strings/pangrams.cpp
+++ strings/pangrams.cpp
@@ -3,11 +3,75 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-using namespace std;
+#include <string>
+#include <ctype.h>
 
+/*
+ * Roy wanted to increase his typing speed for programming
+ * contests. So, his friend advised him to type the sentence
+ * "The quick brown fox jumps over the lazy dog" repeatedly,
+ * because it is a pangram. (Pangrams are sentences constructed
+ * by using every letter of the alphabet at least once.)
+ *
+ * After typing the sentence several times, Roy became bored
+ * with it. So he started to look for other pangrams.
+ *
+ * Given a sentence s, tell Roy if it is a pangram or not.
+ *
+ * Input Format
+ * 	Input consists of a string s.
+ *
+ * Constraints 
+ * 	Length of s can be at most 103 (1≤|s|≤103) and it may
+ * 	contain spaces, lower case and upper case letters.
+ * 	Lower-case and upper-case instances of a letter are
+ * 	considered the same.
+ *
+ * Output Format
+ * 	Output a line containing pangram if s is a pangram, 
+ * 	otherwise output not pangram.
+ *
+ * Sample Input
+ *
+ * Input #1
+ * 	We promptly judged antique ivory buckles for the next prize    
+ *
+ * Input #2
+ * 	We promptly judged antique ivory buckles for the prize    
+ *
+ * Sample Output
+ * Output #1
+ * 	pangram
+ * Output #2
+ * 	not pangram
+ */
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+namespace {
+	bool is_pangram(const std::string& s)
+	{
+		std::vector<bool> chars('z'-'a'+1, false);
+
+		for (auto c : s) {
+            if (!isalpha(c))
+                continue;
+            chars[tolower(c) - 'a'] = true;
+		}
+
+		for (auto char_is_present : chars) {
+			if (!char_is_present)
+				return false;
+		}
+
+		return true;
+	}
+}
+
+int main()
+{
+	std::string s;
+	std::getline (std::cin, s);
+    std::cout << (is_pangram(s) ? "pangram" : "not pangram") << std::endl;
+
     return 0;
 }
 
